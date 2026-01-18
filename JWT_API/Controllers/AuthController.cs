0294d12d80f8bc39cfd6c37012d74df63c8815f0
kaifali98 <kaifali308@gmail.com>
2026-01-18 15:16:21 +0000

    Initial commit

---
+++ JWT_API/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+using JwtApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserDto userDto)
+        {
+            if (userDto.Username != "test" || userDto.Password != "password")
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]!);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, userDto.Username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { Token = tokenString });
+        }
+    }
+}
---
+++ JWT_API/Models/UserDto.cs
@@ -0,0 +1,8 @@
+namespace JwtApi.Models
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
